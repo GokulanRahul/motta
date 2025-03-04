e31cf323794ea5c983d3f8e63a58cff32fa006bf
Gokula Krishnan <137066279+GokulanRahul@users.noreply.github.com>
2025-03-04 12:35:47 +0530

    refrac : change p and print

--- backend_team/Hello.java
+++ backend_team/Hello.java
@@ -12,7 +12,7 @@ public class Hello {
     System.out.println("gokul");
     System.out.println("akash");
     System.out.println("mohan");
-    System.out.println("motta");
+    System.out.print("potta");
 
   }
 }
